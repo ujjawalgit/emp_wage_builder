978ef07720299ad58f7e22c52ebef5d843ef0c09
ujjawalgit <ujjawalkumarsingh08@gmail.com>
2021-04-27 14:45:59 +0530

    wage calculation for a month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -8,29 +8,36 @@ public class EmpWageBuilder {
         final int IS_FULL_TIME = 1;
         final int IS_PART_TIME = 2;
         final int WAGE_RATE = 20;
+        final int MAX_WORKING_DAYS = 20;
 
         int empHr = 0;
+        int totalWorkingHr = 0;
         int salary = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (empCheck) {
+        int day = 1;
+        while (day <= MAX_WORKING_DAYS) {
 
-            case IS_FULL_TIME:
-                System.out.print("Employee is present");
-                empHr = 8;
-                break;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            case IS_PART_TIME:
-                System.out.print("Employee is part time ");
-                empHr = 4;
-                break;
+            switch (empCheck) {
 
-            default:
-                System.out.print("Employee is absent");
-                empHr = 0;
+                case IS_FULL_TIME:
+                    empHr = 8;
+                    break;
+
+                case IS_PART_TIME:
+                    empHr = 4;
+                    break;
+
+                default:
+                    empHr = 0;
+            }
+
+            totalWorkingHr += empHr;
+            day++;
         }
 
-        salary = (WAGE_RATE * empHr);
-        System.out.print("\nsalary:" + salary);
+        salary = (WAGE_RATE * totalWorkingHr);
+        System.out.print("salary:" + salary);
     }
 }
