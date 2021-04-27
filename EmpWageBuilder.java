9917ecd836c9b75392bdecacfbf815e83fd01019
ujjawalgit <ujjawalkumarsingh08@gmail.com>
2021-04-27 13:05:37 +0530

    Welcome message

---
+++ EmpWageBuilder.java
@@ -0,0 +1,8 @@
+package com.empwagebuilder;
+
+public class EmpWageBuilder {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
