a3a92fc02b67d38ad7d2a6dd0af0e8878ca85270
ujjawalgit <ujjawalkumarsingh08@gmail.com>
2021-05-29 10:06:43 +0530

    Added: Compute Employee Wage for multiple companies

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,26 +3,36 @@ package com.empwagebuilder;
 public class EmpWageBuilder {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int WAGE_RATE = 20;
-    public static final int MAX_WORKING_DAYS = 20;
-    public static final int MAX_WORKING_HRS = 100;
 
-<<<<<<< HEAD
+    private final int WAGE_RATE;
+    private final int MAX_WORKING_DAYS;
+    private final int MAX_WORKING_HRS;
+    private final String companyName;
+
+    public EmpWageBuilder(int WAGE_RATE, int MAX_WORKING_DAYS, int MAX_WORKING_HRS, String companyName) {
+        this.WAGE_RATE = WAGE_RATE;
+        this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
+        this.MAX_WORKING_HRS = MAX_WORKING_HRS;
+        this.companyName = companyName;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        //Constants
-        final int IS_FULL_TIME = 1;
-        final int IS_PART_TIME = 2;
-        final int WAGE_RATE = 20;
-        final int MAX_WORKING_DAYS = 20;
-        final int MAX_WORKING_HRS = 100;
+        EmpWageBuilder BharatBiotech = new EmpWageBuilder(500, 24, 240, "BharatBiotech");
+        EmpWageBuilder BharatForge = new EmpWageBuilder(400, 20, 200, "BharatForge");
+
+        System.out.println("\nCompany name:" + BharatBiotech.companyName + "\ntotal empWage:" + BharatBiotech.computeEmpWage());
+        System.out.println("\nCompany name:" + BharatForge.companyName + "\ntotal empWage:" + BharatForge.computeEmpWage());
+    }
+
+    private int computeEmpWage() {
 
         int empHr = 0;
         int totalWorkingHr = 0;
         int salary = 0;
 
         int day = 1;
-        while ((day <= MAX_WORKING_DAYS) && (totalWorkingHr <= MAX_WORKING_HRS)) {
+        while ((day <= this.MAX_WORKING_DAYS) && (totalWorkingHr <= this.MAX_WORKING_HRS)) {
 
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
@@ -36,29 +46,7 @@ public class EmpWageBuilder {
                     empHr = 4;
                     break;
 
-=======
-    public static int computeEmpWage() {
-        int empHr = 0;
-        int totalWorkingHr = 0;
-        int salary = 0;
 
-        int day = 1;
-
-        while ((day <= MAX_WORKING_DAYS) && (totalWorkingHr <= MAX_WORKING_HRS)) {
-
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-
-            switch (empCheck) {
-
-                case IS_FULL_TIME:
-                    empHr = 8;
-                    break;
-
-                case IS_PART_TIME:
-                    empHr = 4;
-                    break;
-
->>>>>>> empwagebuilderUC7
                 default:
                     empHr = 0;
             }
@@ -68,15 +56,7 @@ public class EmpWageBuilder {
         }
 
         salary = (WAGE_RATE * totalWorkingHr);
-<<<<<<< HEAD
-        System.out.print("salary:" + salary);
-=======
         return salary;
     }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        System.out.print( "Total Emp Wage:" + computeEmpWage() );
->>>>>>> empwagebuilderUC7
-    }
 }
+
