adec251cf5c81f8fb2d7b9ba104c08e56c4b77fe
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-05-31 21:30:21 +0530

    Added: Ability to save the Total Wage for Each Company

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -8,6 +8,7 @@ public class EmpWageBuilder {
     private final int MAX_WORKING_DAYS;
     private final int MAX_WORKING_HRS;
     private final String companyName;
+    private int totalWage;
 
     public EmpWageBuilder(int WAGE_RATE, int MAX_WORKING_DAYS, int MAX_WORKING_HRS, String companyName) {
         this.WAGE_RATE = WAGE_RATE;
@@ -20,43 +21,34 @@ public class EmpWageBuilder {
         System.out.println("Welcome to Employee Wage Computation Program");
         EmpWageBuilder BharatBiotech = new EmpWageBuilder(500, 24, 240, "BharatBiotech");
         EmpWageBuilder BharatForge = new EmpWageBuilder(400, 20, 200, "BharatForge");
-
-        System.out.println("\nCompany name:" + BharatBiotech.companyName + "\ntotal empWage:" + BharatBiotech.computeEmpWage());
-        System.out.println("\nCompany name:" + BharatForge.companyName + "\ntotal empWage:" + BharatForge.computeEmpWage());
+        /* calling method to compute totalWage for a company*/
+        BharatBiotech.computeEmpWage();
+        BharatForge.computeEmpWage();
+        System.out.println("\nCompany name:" + BharatBiotech.companyName + "\ntotal empWage:" + BharatBiotech.totalWage);
+        System.out.println("\nCompany name:" + BharatForge.companyName + "\ntotal empWage:" + BharatForge.totalWage);
     }
 
-    private int computeEmpWage() {
-
+    /*function to compute total wage  for company*/
+    public void computeEmpWage() {
         int empHr = 0;
         int totalWorkingHr = 0;
-        int salary = 0;
-
         int day = 1;
         while ((day <= this.MAX_WORKING_DAYS) && (totalWorkingHr <= this.MAX_WORKING_HRS)) {
-
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-
             switch (empCheck) {
-
                 case IS_FULL_TIME:
                     empHr = 8;
                     break;
-
                 case IS_PART_TIME:
                     empHr = 4;
                     break;
-
-
                 default:
                     empHr = 0;
             }
-
             totalWorkingHr += empHr;
             day++;
         }
-
-        salary = (WAGE_RATE * totalWorkingHr);
-        return salary;
+        this.totalWage = (WAGE_RATE * totalWorkingHr); // saving total wage to instance variable
     }
 }
 
